036fbf4cb9db77ab50b9039c09b22b4d145a381d
akihirao <akihirao@gmail.com>
2026-01-17 23:10:35 +0900

    minor rev

--- R/graphics.R
+++ R/graphics.R
@@ -26,14 +26,14 @@ plot_typical_seasonal_cycle <- function(ts){
                                   ) +
     geom_point(size = 2) +
     geom_line(linetype= "dashed") +
-    labs(y = paste0("Monthly average",expression(Temperature~(degree*C)))) +
+    labs(title="Seasonal trend of monthly average temperature",
+      y = expression(Temperature~(degree*C))) +
     scale_x_discrete(
       labels = function(x) sprintf("%02d", as.integer(x))
     )
 
 
   plot(plot_monthly_ave_temp )
-  #return(plot_monthly_ave_temp)
 
 }
 
@@ -72,8 +72,129 @@ plot_temp_dev <- function(ts){
   ggtitle("Temperature anomalies")
 
   plot(dev_plot)
-  #return(dev_plot)
+
   
 }
 
 
+#' Function to plot estimated components: level, drift, seasonal, and auto-regression
+#' @import ggplot2
+#' @param ts temperature time series object
+#'
+#' @encoding UTF-8
+#'
+#' @export
+#'
+plot_level_trend_season_ar <- function(res){
+
+  alpha_hat <- res[[2]]$alphahat
+
+  model_level_plot <- forecast::autoplot(alpha_hat[,"level"]) + 
+    labs(y = "", x = "Time") + 
+    ggtitle("Level component")
+
+  model_slope_plot <- forecast::autoplot(alpha_hat[,"slope"]) +
+    labs(y = "", x = "Time") +
+    ggtitle("Drift component")
+
+  model_season_plot <- forecast::autoplot(alpha_hat[,"sea_dummy1"]) +
+    labs(y = "", x = "Time") +
+    ggtitle("Seasonal component")
+
+  model_arima1_plot <- forecast::autoplot(alpha_hat[,"arima1"]) +
+    labs(y = "", x = "Time") +
+    ggtitle("Auto-regression component")
+
+   level_drift_seaon_ar_plots <- list(
+    model_level_plot,
+    model_slope_plot,
+    model_season_plot,
+    model_arima1_plot
+    )
+
+   return(level_drift_seaon_ar_plots)
+
+}
+
+
+
+#' Function to plot level component with confidence interval
+#' @import ggplot2
+#' @param ts temperature time series object
+#'
+#' @encoding UTF-8
+#'
+#' @export
+#'
+plot_level_ci <- function(res,ci_range=0.95){
+
+  smooth <- res[[2]] # smoothing
+  alpha_hat <- smooth$alphahat
+
+  ci_lab <- paste0(round(ci_range*100,0),"% CI")
+  level <- alpha_hat[,"level"]
+  ci <- confint(res[[2]], level = ci_range)
+
+  level_tidy <- cbind(
+    data.frame(time=time(level),
+               level=level),
+  as.data.frame(ci$level)
+  ) %>%
+  as_tibble()
+
+  level_plot <- ggplot(data=level_tidy,
+                         aes(x=time,y=level)) +
+    labs(title=paste0("Level component (grey area: ",ci_lab),
+       x="Year", y="Temperature") +
+    geom_line(aes(y=level), size = 1.2) +
+    geom_ribbon(aes(ymin = lwr, ymax = upr), alpha = 0.3)
+
+  return(level_plot)
+
+}
+
+
+
+
+
+#' Function to plot drift component with confidence interval
+#' @import ggplot2
+#' @param ts temperature time series object
+#'
+#' @encoding UTF-8
+#'
+#' @export
+#'
+plot_drift_ci <- function(res,ci_range=0.95){
+
+  ci <- confint(res[[2]], level = ci_range)
+  smooth <- res[[2]] # smoothing
+  alpha_hat <- smooth$alphahat
+
+  drift <- alpha_hat[,"slope"]
+
+  mean_drift_year <- mean(drift) * 12
+  
+
+  drift_tidy <- cbind(
+    data.frame(time=time(drift),
+               level=drift),
+  as.data.frame(ci$slope)
+  ) %>%
+  as_tibble()
+
+  ci_lab <- paste0("Drift component (grey area: ", round(ci_range*100,0),"% CI")
+  sub_lab <- paste0("Average drift rate per year = " ,mean_drift_year)
+
+  level_plot <- ggplot(data=level_tidy,
+                         aes(x=time,y=drift)) +
+    labs(title=ci_lab,
+      subtitle= sub_lab,
+       x="Year", y="Drift") +
+    geom_line(aes(y=level), size = 1.2) +
+    geom_ribbon(aes(ymin = lwr, ymax = upr), alpha = 0.3) +
+    geom_hline(yintercept=0, linetype="dashed") 
+
+  return(drift_plot)
+
+}
