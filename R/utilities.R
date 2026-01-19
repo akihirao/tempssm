dc6704b1cc97f3b9981e9393025d51a9f0acd602
akihirao <akihirao@gmail.com>
2026-01-19 17:03:53 +0900

    update

--- R/utilities.R
+++ R/utilities.R
@@ -1,14 +1,25 @@
 
-#' Function to convert from data.csv to monthly time sieries object
-#' @import readr
+#' Convert a monthly temperature CSV file to a monthly time series object
 #'
-#' @param csv input csv file
+#' @details
+#' The input CSV file must have the following format:
+#'
+#' \preformatted{
+#' Year,Month,Temp
+#' 2001,1,10.4
+#' 2001,2,8.2
+#' 2001,3,NA
+#' 2001,4,13.6
+#' ...
+#' }
+#'
+#' @param csv Path to a CSV file containing monthly temperature data.
+#'
+#' @importFrom readr read_csv
 #'
 #' @encoding UTF-8
 #'
 #' @export
-
-
 monthly_csv2ts <- function(csv) {
 
   raw_data <- readr::read_csv(csv)
@@ -18,18 +29,19 @@ monthly_csv2ts <- function(csv) {
     as.matrix(raw_data["Temp"]),
     start = c(raw_data$Year[1], raw_data$Month[1]),
     frequency = 12
-    )
+  )
 
   colnames(Temp_ts) <- "Temp"
-  return(Temp_ts)
+  Temp_ts
 }
 
 
 
-#' Function to convert from dairy zoo object to monthly ts object
+#' Convert dairy zoo object to monthly ts object
+#'
 #' @import zoo
 #'
-#' @param csv input csv file
+#' @param zoo zoo object
 #'
 #' @encoding UTF-8
 #'
@@ -66,6 +78,52 @@ zoo_daily2ts_monthly <- function(zoo, var = "Temp", na.rm = TRUE) {
 
   return(ts_monthly)
 
+}
+
+
+
+#' Generate a monthly temperature anomaly time series
+#'
+#' Temperature anomalies are calculated by subtracting the long-term
+#' monthly mean (climatology) from each observation.
+#'
+#' @param temp_ts A monthly temperature time series object of class \code{ts}.
+#'
+#' @details
+#' Monthly climatology is calculated as the mean temperature for each calendar
+#' month. Missing values are ignored.
+#'
+#' @return A \code{ts} object of monthly temperature anomalies.
+#'
+#' @examples
+#' temp_ts <- ts(
+#'   c(5.2, 6.1, 9.3, 13.5, 17.8, 21.0,
+#'     24.3, 25.1, 22.0, 17.1, 11.2, 7.0),
+#'   start = c(2000, 1),
+#'   frequency = 12
+#' )
+#'
+#' temp_dev <- generate_temp_dev(temp_ts)
+#' tapply(as.numeric(temp_dev), cycle(temp_dev), mean)
+#'
+#' @export
+generate_temp_dev <- function(temp_ts) {
+
+  stopifnot(inherits(temp_ts, "ts"))
+  stopifnot(frequency(temp_ts) == 12)
+
+  temp <- as.numeric(temp_ts)
+  mnum <- cycle(temp_ts)
+  mfac <- factor(mnum, levels = 1:12)
+
+  monthly_ave_temp <- tapply(temp, mfac, mean, na.rm = TRUE)
+  clim <- monthly_ave_temp[as.integer(mfac)]
+
+  ts(
+    temp - clim,
+    start = start(temp_ts),
+    frequency = frequency(temp_ts)
+  )
 }
 
 
