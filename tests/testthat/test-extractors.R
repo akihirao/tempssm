7ed17d6ddeece979e30a6f0db306e253878babb8
akihirao <akihirao@gmail.com>
2026-04-27 23:34:59 +0900

    bug fix test

--- tests/testthat/test-extractors.R
+++ tests/testthat/test-extractors.R
@@ -2,8 +2,9 @@
 
 test_that("extract_level_ts returns ts of correct length", {
 
-  level <- extract_level_ts(res_tempssm)
+  res_temp_test <- tempssm(temp_ts_test)
+  level <- extract_level_ts(res_temp_test)
 
   expect_s3_class(level, "ts")
-  expect_equal(length(level), length(niigata_sst))
+  expect_equal(length(level), length(temp_ts_test))
 })
