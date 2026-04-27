ea34bac30268854c44a6bf50364fdc529d285a67
akihirao <akihirao@gmail.com>
2026-04-27 23:53:49 +0900

    bug fix test

--- tests/testthat/setup-tempssm.R
+++ tests/testthat/setup-tempssm.R
@@ -1,4 +1,15 @@
 # tests/testthat/setup-tempssm.R
 
-data(niigata_sst)
-res_tempssm <- tempssm(niigata_sst)
+set.seed(123)
+
+temp_ts_test <- ts(
+  10 +
+    seq(0, 1, length.out = 120) +      # weak trend
+    rep(rnorm(12, 0, 0.5), 10) +       # seasonal cycle
+    rnorm(120, 0, 0.3),                # noise
+  start = c(2000, 1),
+  frequency = 12
+)
+
+
+res_tempssm <- tempssm(temp_ts_test)
--- tests/testthat/test-extractors.R
+++ tests/testthat/test-extractors.R
@@ -2,8 +2,7 @@
 
 test_that("extract_level_ts returns ts of correct length", {
 
-  res_temp_test <- tempssm(temp_ts_test)
-  level <- extract_level_ts(res_temp_test)
+  level <- extract_level_ts(res_tempssm)
 
   expect_s3_class(level, "ts")
   expect_equal(length(level), length(temp_ts_test))
